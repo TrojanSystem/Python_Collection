cb6030493bc25236d780a6046e571ae699f604c6
TrojanSystem <suretion747@gmail.com>
2022-12-09 11:30:43 +0300

    Collection Python Example From 100 days of code the complete python pro bootcamp

--- pythonProject/upto_day_8.py
+++ pythonProject/upto_day_8.py
@@ -3,11 +3,23 @@ class UptoDay8:
     def project_day_8():
         totalEven = 0
         totalOdd = 0
-        for evens in range(1,101):
-            if evens % 2 == 0:
-                totalEven += evens
+        print('Which Program U want to Run')
+        which = input('Type "EvenOdd", "FizzBuzz" program: ')
+        if which == 'EvenOdd':
+            for evens in range(1, 101):
+                if evens % 2 == 0:
+                    totalEven += evens
 
-            else:
+                else:
 
-                totalOdd += evens
-        print(f"Total Even {totalEven} : Total Odd {totalOdd}")
+                    totalOdd += evens
+            print(f"Total Even {totalEven} : Total Odd {totalOdd}")
+        elif which == 'FizzBuzz':
+            for evens in range(1, 101):
+                if evens % 3 == 0:
+                    print('Fizz')
+
+                elif evens % 5 == 0:
+                    print('Buzz')
+                else:
+                    print(evens)
